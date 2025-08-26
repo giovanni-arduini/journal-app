5b887ca81245d5bdd82c4afdc98170debec8547d
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-26 22:57:19 +0200

    ADD PostCard element component

---
+++ src/components/PostCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({ post: { type: Object, required: true } });
+const post = props.post;
+</script>
+
+<template>
+  <div class="bg-white">
+    <h1>{{ post.name }}</h1>
+    <figure>
+      <img src="" alt="immagine" />
+    </figure>
+    <p>{{ post.date }}</p>
+
+    <button style="color: orange">
+      {{ post.special ? "★" : "☆" }}
+    </button>
+  </div>
+</template>
