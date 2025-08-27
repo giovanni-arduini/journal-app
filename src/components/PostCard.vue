f039acf2b097db391cf738075d78cfb8730d001d
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-27 08:33:05 +0200

    ADD refined style for each rendered card

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,23 +1,46 @@
 <script setup>
+import { ref, onMounted } from "vue";
 const props = defineProps({ post: { type: Object, required: true } });
 const post = props.post;
+
+// Stato locale per tipo decorazione
+const decorationType = ref("scotch");
+const rotation = ref(0);
+
+onMounted(() => {
+  decorationType.value = Math.random() < 0.5 ? "scotch" : "pin";
+  rotation.value = Math.random() * 20 - 10;
+});
 </script>
 
 <template>
-  <!-- Contenitore esterno più grande -->
-  <div class="relative flex flex-col items-center w-72 h-96 mx-auto">
-    <!-- Scotch angolo sinistro -->
-    <div
-      class="absolute -left-5 top-2 w-24 h-8 bg-yellow-200 rotate-[-45deg] rounded opacity-80 shadow-md z-10"
-    ></div>
-    <!-- Scotch angolo destro -->
-    <div
-      class="absolute -right-4 top-2 w-24 h-8 bg-yellow-200 rotate-[45deg] rounded opacity-80 shadow-md z-10"
-    ></div>
+  <div
+    class="relative flex flex-col items-center w-72 h-96 mx-auto"
+    :style="{ transform: `rotate(${rotation}deg)` }"
+  >
+    <!-- Scotch -->
+    <template v-if="decorationType === 'scotch'">
+      <div
+        class="absolute -left-5 top-2 w-24 h-8 bg-yellow-200 rotate-[-45deg] rounded opacity-80 shadow-md z-10"
+      ></div>
+      <div
+        class="absolute -right-4 top-2 w-24 h-8 bg-yellow-200 rotate-[45deg] rounded opacity-80 shadow-md z-10"
+      ></div>
+    </template>
+    <!-- Puntina -->
+    <template v-else>
+      <div
+        class="absolute left-1/2 -translate-x-1/2 top-1 z-10 flex flex-col items-center"
+      >
+        <div
+          class="w-7 h-7 bg-red-600 rounded-full border-gray-200 border-2 shadow-md"
+        ></div>
+        <!-- <div class="w-1 h-5 bg-gray-400 rounded-b-full"></div> -->
+      </div>
+    </template>
 
-    <!-- Card polaroid più piccola, centrata -->
     <div
-      class="flex flex-col items-center w-64 bg-white rounded-lg shadow-lg pb-6 pt-4 mx-auto z-0"
+      class="flex flex-col items-center w-64 bg-white rounded-lg shadow-lg pb-6 pt-4 mx-auto z-0 relative"
     >
       <figure
         class="w-full h-64 flex items-center justify-center overflow-hidden rounded-t-lg p-2"
