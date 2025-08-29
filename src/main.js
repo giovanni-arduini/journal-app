440740ff8d98cb01d07ac97d24c66679fe45e4c0
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-29 19:01:02 +0200

    ADD edito form component

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import PostCard from "./components/PostCard.vue";
 import TagInput from "./components/TagInput.vue";
 import PostDetail from "./components/PostDetail.vue";
 import NewPostForm from "./components/NewPostForm.vue";
+import EditPostForm from "./components/EditPostForm.vue";
 
 const app = createApp(App);
 
@@ -18,5 +19,6 @@ app.component("PostCard", PostCard);
 app.component("BasicModal", BasicModal);
 app.component("PostDetail", PostDetail);
 app.component("NewPostForm", NewPostForm);
+app.component("EditPostForm", EditPostForm);
 
 app.mount("#app");
